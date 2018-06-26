34aa0f358375399b708e05a1c88027f29ef308f7
ddy250 <odedsagi@gmail.com>
2018-06-26 14:15:37 +0300

    version3

--- routes/resources.js
+++ routes/resources.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('resources', { title: 'Resources' });
+    res.render('resources', { title: 'resources' });
 });
 
 module.exports = router;
