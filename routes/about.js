881f7cf81b3d4149ff7b3bdd929993742ae8e004
ddy250 <odedsagi@gmail.com>
2018-07-03 10:50:15 +0300

    version4.2

--- routes/about.js
+++ routes/about.js
@@ -4,9 +4,8 @@ var request = require('request');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    request('');
-
     request('https://hidden-retreat-94036.herokuapp.com/youtube-list', function (error, response, body) {
+        var c;
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         console.log('body:', body); // Print the HTML for the Google homepage.
